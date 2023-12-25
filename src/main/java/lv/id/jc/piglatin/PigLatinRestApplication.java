cfdd609bb78517e26bf0c6d436842b6842c39522
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-12-25 18:09:13 +0200

    Initial commit

---
+++ src/main/java/lv/id/jc/piglatin/PigLatinRestApplication.java
@@ -0,0 +1,13 @@
+package lv.id.jc.piglatin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigLatinRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigLatinRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lv/id/jc/piglatin/PigLatinRestApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.id.jc.piglatin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigLatinRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
