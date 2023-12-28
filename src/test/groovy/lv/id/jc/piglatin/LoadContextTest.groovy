2d32b8aa7ceff8d25cbc7cb806d1f9ba7c10dba5
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-12-28 23:10:25 +0200

    fix: LoadContextTest

--- src/test/groovy/lv/id/jc/piglatin/LoadContextTest.groovy
+++ src/test/groovy/lv/id/jc/piglatin/LoadContextTest.groovy
@@ -1,24 +1,18 @@
 package lv.id.jc.piglatin
 
-import lv.id.jc.piglatin.controller.PigLatinController
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
 import spock.lang.Specification
 
 @SpringBootTest
 class LoadContextTest extends Specification {
-
-    @Autowired(required = false)
-    private PigLatinController webController
+    @Autowired
+    private ApplicationContext applicationContext
 
     def 'context loads'() {
         expect:
-        true
+        applicationContext.isRunning()
     }
-
-    def 'all expected beans are created'() {
-        expect: "the WebController is created"
-        webController
-    }
-
 }
