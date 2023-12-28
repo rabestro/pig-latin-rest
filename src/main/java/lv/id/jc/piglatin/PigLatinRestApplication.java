25aa51fb0717327fbc0fb6c66600261784248c45
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-12-29 00:01:06 +0200

    fix: context

--- src/main/java/lv/id/jc/piglatin/PigLatinRestApplication.java
+++ src/main/java/lv/id/jc/piglatin/PigLatinRestApplication.java
@@ -2,24 +2,11 @@ package lv.id.jc.piglatin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @SpringBootApplication
 public class PigLatinRestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PigLatinRestApplication.class, args);
-    }
-
-    @Bean
-    public LocalValidatorFactoryBean validator() {
-        return new LocalValidatorFactoryBean();
-    }
-
-    @Bean
-    public MethodValidationPostProcessor validationPostProcessor() {
-        return new MethodValidationPostProcessor();
     }
 }
