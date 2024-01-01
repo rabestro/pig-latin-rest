9043530c7d5a9872118e9cfa80c64e226cbe5adc
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-01-01 23:39:39 +0200

    chore: update SpockConfig.groovy

--- src/test/resources/SpockConfig.groovy
+++ src/test/resources/SpockConfig.groovy
@@ -2,7 +2,9 @@ report {
     issueNamePrefix 'Issue #'
     issueUrlPrefix 'https://github.com/rabestro/pig-latin-rest/issues/'
 }
-
+unroll {
+    includeFeatureNameForIterations false
+}
 spockReports {
     set(['com.athaydes.spockframework.report.showCodeBlocks'                    : true,
          'com.athaydes.spockframework.report.outputDir'                         : './target/site/spock-reports',
@@ -11,4 +13,10 @@ spockReports {
          'com.athaydes.spockframework.report.internal.HtmlReportCreator.enabled': true,
          'com.athaydes.spockframework.report.IReportCreator'                    : 'com.athaydes.spockframework.report.internal.HtmlReportCreator'
     ])
+}
+
+// https://blog.solidsoft.pl/2022/04/05/more-compact-parameterized-tests-reporting-in-spock-2.1/
+
+static boolean isExecutedFromIdea() {
+    return System.getProperty("java.class.path").contains("idea_rt.jar")    //any more reliable way?
 }
