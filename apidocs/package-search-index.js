1469f8b22ac5405997f4eda28dcd6fdaa34c23d0
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-03-09 00:04:35 +0200

    Site checkin for project Pig Latin Translator

--- apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"lv.id.jc.piglatin"},{"l":"lv.id.jc.piglatin.actuator"},{"l":"lv.id.jc.piglatin.api"},{"l":"lv.id.jc.piglatin.controller"},{"l":"lv.id.jc.piglatin.core"},{"l":"lv.id.jc.piglatin.model"},{"l":"lv.id.jc.piglatin.service"}];updateSearchResults();
